b6186f522038120e4943b6a88c12a259bccb35bb
ninja03 <ninja03>
2024-05-06 11:31:54 +0900

    symbol

--- server/server.rb
+++ server/server.rb
@@ -5,7 +5,7 @@ require "sinatra/json"
 require 'cairo'
 
 def load_sample_question
-  JSON.load(File.read('question.json'))
+  JSON.load(File.read('question.json'), nil, symbolize_names: true, create_additions: false)
 end
 
 configure do
@@ -28,26 +28,26 @@ get "/random" do
   start = Array.new(board_height) { Array.new(board_width) { rand(4) }.join }
   goal = Array.new(board_height) { Array.new(board_width) { rand(4) }.join }
   general_patterns = (1..rand(1..3)).map do |i|
-    pattern_width = rand(1..10),
+    pattern_width = rand(1..10)
     pattern_height = rand(1..10)
     {
-      "p" => 25 + i,
-      "width" => pattern_width,
-      "height" => pattern_height,
-      "cells" => Array.new(pattern_height) { Array.new(pattern_width) { rand(2) }.join }
+      p: 25 + i,
+      width: pattern_width,
+      height: pattern_height,
+      cells: Array.new(pattern_height) { Array.new(pattern_width) { rand(2) }.join }
     }
   end
 
   $question = {
-    "board" => {
-      "width" => board_width,
-      "height" => board_height,
-      "start" => start,
-      "goal" => goal
+    board: {
+      width: board_width,
+      height: board_height,
+      start: start,
+      goal: goal
     },
-    "general" => {
-      "n" => general_patterns.size,
-      "patterns" => general_patterns
+    general: {
+      n: general_patterns.size,
+      patterns: general_patterns
     }
   }
 
@@ -63,23 +63,23 @@ post '/api/answer' do
 end
 
 get '/image/start' do
-  board = $question['board']
+  board = $question[:board]
   colors = ["pink", "lightblue", "lightgreen", "orange"]
-  create_image(board['width'], board['height'], board["goal"], colors)
+  create_image(board[:width], board[:height], board[:goal], colors)
 end
 
 get "/image/goal" do
-  board = $question['board']
+  board = $question[:board]
   colors = ["pink", "lightblue", "lightgreen", "orange"]
-  create_image(board['width'], board['height'], board["goal"], colors)
+  create_image(board[:width], board[:height], board[:goal], colors)
 end
 
 get "/image/pattern/:p" do
-  pattern = $question['general']['patterns'].find do |p|
-    p["p"] == params[:p].to_i
+  pattern = $question[:general][:patterns].find do |p|
+    p[:p] == params[:p].to_i
   end
   colors = ["lightgray", "black"]
-  create_image( pattern["width"], pattern["height"], pattern["cells"], colors)
+  create_image( pattern[:width], pattern[:height], pattern[:cells], colors)
 end
 
 def create_image(board_width, board_height, data, colors)
@@ -95,6 +95,7 @@ def create_image(board_width, board_height, data, colors)
       end
     end
     surface.write_to_png(f.path)
+    content_type :png
     File.binread(f.path)
   end
 end
